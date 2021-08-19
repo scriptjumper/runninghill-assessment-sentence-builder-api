54c27d2d621dcbff380a361a5a246932f51c278a
Shaeen Singh <shaeenkevinsingh@gmail.com>
2021-08-19 13:00:24 +0200

    Migration data for word types

--- database.sql
+++ database.sql
@@ -4,3 +4,14 @@ CREATE TABLE types (
 	id serial PRIMARY KEY,
 	title VARCHAR ( 50 ) 
 );
+
+INSERT INTO types (title)
+VALUES ('Noun'),
+('Verb'),
+('Adjective'),
+('Adverb'),
+('Pronoun'),
+('Preposition'),
+('Conjunction'),
+('Determiner'),
+('Exclamation');
