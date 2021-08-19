72bcbd5274b289f1db2947e8e39ebe96b1278832
Shaeen Singh <shaeenkevinsingh@gmail.com>
2021-08-19 13:57:08 +0200

    Getting all the word types

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const pool = require('./db')
 
+app.use(express.json()) // => res.body
+
+//ROUTES//
+
+// get all word types
+app.get('/wordTypes', async (req, res) => {
+  try {
+    const wordTypes = await pool.query('SELECT * FROM types')
+
+    res.json(wordTypes.rows)
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
 app.listen(8000, () => {
   console.log('Server is listening on port 8000')
 })
