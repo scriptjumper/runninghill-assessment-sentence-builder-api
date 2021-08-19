61d30099f80133299613120ab254eb20583f5f77
Shaeen Singh <shaeenkevinsingh@gmail.com>
2021-08-19 14:14:58 +0200

    Created a endpoint to fetch all words under a word type using the word type id

--- index.js
+++ index.js
@@ -6,12 +6,31 @@ app.use(express.json()) // => res.body
 
 //ROUTES//
 
-// get all word types
+/**
+ * Getting all the word types
+ */
 app.get('/wordTypes', async (req, res) => {
   try {
     const wordTypes = await pool.query('SELECT * FROM types')
 
     res.json(wordTypes.rows)
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
+/**
+ * Getthing all words by word type id
+ *
+ * @param id - is the word type id
+ */
+app.get('/words/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const words = await pool.query('SELECT * FROM words WHERE fk_type_id = $1', [id])
+
+    res.json(words.rows)
   } catch (error) {
     console.error(error.message)
   }
